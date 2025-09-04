d0fde8be51a1dcbc7948836ec4524e9ec13e15d0
Suryadevara Pavan <suryadevarapavan770@gmail.com>
2025-09-04 10:57:58 +0530

    Added game.py which has gamelogic

---
+++ game.py
@@ -0,0 +1,3 @@
+print("Welcome to CELNEX")
+ipchoice=int(input("Celnex – Heroes from all worlds battle for control and survival.\n1.A.V[The Last-Machine]\n2.MARIA[Heir of Glory]\nChoose one Character:"))
+print(ipchoice)
