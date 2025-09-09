fe2f65bf9ea2e854acf93b9f7c7721eef6c7a3be
Suryadevara Pavan <suryadevarapavan770@gmail.com>
2025-09-09 06:47:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
-
 from flask import Flask, request, redirect, render_template, url_for
 from pymongo import MongoClient
 
 app = Flask(__name__)
 
-# Connect to MongoDB (local or replace with your Mongo URI)
 client = MongoClient('mongodb://localhost:27017/')
 db = client['celnex']
 users_collection = db['cred']
@@ -12,15 +10,20 @@ users_collection = db['cred']
 @app.route("/", methods=["GET", "POST"])
 def login():
     if request.method == "POST":
-        user_id = request.form.get("user_id")
-        user = users_collection.find_one({"user_id": user_id})
-        
+        try:
+            user_id = int(request.form.get("user_id"))
+        except ValueError:
+            return redirect(url_for('login', error=1))
+
+        user = users_collection.find_one({"pid": user_id})
+
         if user:
             return f"<h2>Welcome, {user_id}!</h2><p>You have successfully logged in.</p>"
         else:
             return redirect(url_for('login', error=1))
     
     return render_template("index.html")
-    
+
 if __name__ == "__main__":
     app.run(debug=True)
+
